22cd5e04836cd7ef039112193d090f2e15a32adc
zoro <royalstar2586@gmail.com>
2025-12-06 17:44:22 +0530

    minor fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.brew_brother_cafe"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
